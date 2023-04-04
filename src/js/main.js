b421567f14043fd38fb3c4379f3519711aa9f89f
RYSMENDEEVA <rysmendeevaadinai@gmail.com>
2023-04-04 14:20:11 +0600

    add: link

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,22 @@
+let tabs = document.querySelectorAll(".tabs__tab-nav-btn")
+let tabContent = document.querySelectorAll(".tabs__content-item")
 
+tabs.forEach(el => {
+    el.addEventListener("click",() => {
+      let item = el.getAttribute("data-tab")
+      let current = document.getElementById(item)
+     tabs.forEach(el => {
+         el.classList.remove("active")
+     })
+     tabContent.forEach(el => {
+        el.classList.remove('active')
+     })
+    el.classList.add("active")
+    console.log(current)
+    current.classList.add("active")
+})
+
+})
 
 
 
@@ -28,7 +46,6 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('show')
 
 })
-
+new WOW().init();
 AOS.init();
-
 
